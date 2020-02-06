870b9d55499cb90503eca1a11cb0f1dd46bcf396
akozyreva <anna.ko4523546@gmail.com>
2020-02-06 15:50:08 +0100

    fix errors in music file

--- MiniMiniMusicApp.java
+++ MiniMiniMusicApp.java
@@ -3,14 +3,14 @@ import javax.sound.midi.*;
 public class MiniMiniMusicApp {
     public void play() {
         try{
-            Sequencer player = MidiSystem.getSequencer(false);
+            Sequencer player = MidiSystem.getSequencer();
             // open synthesizer
             player.open();
             Sequence seq = new Sequence(Sequence.PPQ, 4);
             Track track = seq.createTrack();
             MidiEvent event = null;
             ShortMessage a = new ShortMessage();
-            a.setMessage(128, 1, 44, 100);
+            a.setMessage(144, 1, 44, 100);
             MidiEvent noteOn = new MidiEvent(a, 1);
             track.add(noteOn);
 
